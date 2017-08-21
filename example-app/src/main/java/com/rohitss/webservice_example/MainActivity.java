f66a7d73e3e861e53b09d884aebe05fff30aa6ee
RohitSS <RohitSurwase@users.noreply.github.com>
2017-08-22 00:14:18 +0530

    adding library example

--- example-app/src/main/java/com/rohitss/webservice_example/MainActivity.java
+++ example-app/src/main/java/com/rohitss/webservice_example/MainActivity.java
@@ -1,12 +1,69 @@
 package com.rohitss.webservice_example;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.rohitss.api_calling_flow.ApiCallingFlow;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.btnRequestApi)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestTestApi();
+            }
+        });
+    }
+
+    private void requestTestApi() {
+        RelativeLayout parentLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        final ApiCallingFlow apiCallingFlow = new ApiCallingFlow(this, parentLayout, false) {
+            @Override
+            public void callCurrentApiHere() {
+                requestTestApi();
+            }
+        };
+        AndroidNetworking.get("https://jsonplaceholder.typicode.com/posts/1")
+                .setPriority(Priority.HIGH)
+                .setTag("requestTestApi")
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("MainActivity", "onResponse: " + response);
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                apiCallingFlow.onSuccessResponse();
+                            }
+                        }, 5000);
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("MainActivity", "onError Code: " + anError.getErrorCode());
+                        Log.d("MainActivity", "onError Body: " + anError.getErrorBody());
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                apiCallingFlow.onErrorResponse();
+                            }
+                        }, 5000);
+                    }
+                });
     }
 }
---
+++ example-app/src/main/java/com/rohitss/webservice_example/MyApplication.java
@@ -0,0 +1,16 @@
+package com.rohitss.webservice_example;
+
+import android.app.Application;
+
+import com.androidnetworking.AndroidNetworking;
+
+/**
+ * Created by JayYogeshwar on 21-08-2017.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AndroidNetworking.initialize(getApplicationContext());
+    }
+}
